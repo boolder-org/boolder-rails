3bb06ae63245db2702df5f5afbb293882524f9cb
Nicolas Mondollot <nmondollot@gmail.com>
2021-01-20 17:12:41 +0100

    Refactor topo edit

--- app/helpers/default_form_builder.rb
+++ app/helpers/default_form_builder.rb
@@ -13,4 +13,9 @@ class DefaultFormBuilder < ActionView::Helpers::FormBuilder
     html_options.reverse_merge! class: "mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm"
     super(method, choices, options, html_options)
   end
+
+  def text_area(attribute, options = {})
+    options.reverse_merge! class: "mt-1 focus:ring-green-500 focus:border-green-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+    super(attribute, options)
+  end
 end
