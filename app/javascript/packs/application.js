edba5dd3636a5ac19762117025e58ea7f4b90f99
Nicolas Mondollot <nmondollot@gmail.com>
2021-01-26 17:15:03 +0100

    Remove turbolinks, upgrade to turbo

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,8 +3,6 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
@@ -24,3 +22,7 @@ require("stylesheets/application.scss")
 
 // Stimulus controllers
 import "controllers"
+
+// $(document).on("turbo:load"), () => {
+// 	console.log("Turbo!")
+// }
