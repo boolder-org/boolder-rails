4d830cce45462c512c7dec0c60d2fd537b4a89e5
Nicolas Mondollot <nmondollot@gmail.com>
2021-01-27 20:04:34 +0100

    Problem index: refresh when filters change

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -1,7 +1,14 @@
 class ProblemsController < ApplicationController
   def index
   	@area = Area.find(params[:area_id])
-  	@problems = @area.problems.joins(:circuit).where(circuits: { color: params[:color].presence || "blue" })
+  	@problems = @area.problems
+
+  	if circuit_id = params.dig(:search, :circuit_id).presence
+  		@problems = @problems.where(circuit_id: circuit_id)
+  		@problems = @problems.sort_by{|p| p.enumerable_circuit_number }
+  	else 
+  		@problems = @problems.order("grade ASC")
+  	end
   end
 
   def show
