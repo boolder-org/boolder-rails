4d6008d0770b19a6d23130343c9cb58db97be763
Nicolas Mondollot <nmondollot@gmail.com>
2021-01-29 10:28:53 +0100

    Fuck tabs

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -1,12 +1,13 @@
 class ProblemsController < ApplicationController
   def index
-  	@area = Area.find(params[:area_id])
-  	@problems = @area.problems.order("grade ASC, id ASC")
+    @area = Area.find(params[:area_id])
+    @problems = @area.problems.order("grade ASC, id ASC")
 
-  	if params[:level] == "level1"
-  		@problems = @problems.level1
-  	elsif params[:level] == "level2"
-  		@problems = @problems.level2
+    # FIXME: make it DRY
+    if params[:level] == "level1"
+      @problems = @problems.level1
+    elsif params[:level] == "level2"
+      @problems = @problems.level2
     elsif params[:level] == "level3"
       @problems = @problems.level3
     elsif params[:level] == "level4"
@@ -25,17 +26,6 @@ class ProblemsController < ApplicationController
 
     @page = params[:page].presence || 1
     @problems = @problems.page(@page).per(100)
-
-    # sleep 2.seconds
-
-    # @problems = @problems.take(12)
-
-  	# if circuit_id = params.dig(:search, :circuit_id).presence
-  	# 	@problems = @problems.where(circuit_id: circuit_id)
-  	# 	@problems = @problems.sort_by{|p| p.enumerable_circuit_number }
-  	# else 
-  	# 	@problems = @problems.order("grade ASC")
-  	# end
   end
 
   def show
