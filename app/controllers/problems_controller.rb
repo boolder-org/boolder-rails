e54df7b5dca46b570e50d08a0394a19c17c926cc
Nicolas Mondollot <nmondollot@gmail.com>
2021-01-29 10:10:42 +0100

    Problems index level filter

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -3,15 +3,25 @@ class ProblemsController < ApplicationController
   	@area = Area.find(params[:area_id])
   	@problems = @area.problems.order("grade ASC, id ASC")
 
-  	if params[:level] == "beginner"
-  		@problems = @problems.beginner
-  	elsif params[:level] == "intermediate"
-  		@problems = @problems.intermediate
-    elsif params[:level] == "advanced"
-      @problems = @problems.advanced
+  	if params[:level] == "level1"
+  		@problems = @problems.level1
+  	elsif params[:level] == "level2"
+  		@problems = @problems.level2
+    elsif params[:level] == "level3"
+      @problems = @problems.level3
+    elsif params[:level] == "level4"
+      @problems = @problems.level4
+    elsif params[:level] == "level5"
+      @problems = @problems.level5
+    elsif params[:level] == "level6"
+      @problems = @problems.level6
+    elsif params[:level] == "level7"
+      @problems = @problems.level7
+    elsif params[:level] == "level8"
+      @problems = @problems.level8
     else
       params[:level] = "all"
-  	end
+    end
 
     @page = params[:page].presence || 1
     @problems = @problems.page(@page).per(100)
