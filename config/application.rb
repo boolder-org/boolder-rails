304c084a7b2c1ebfb1c25bbf04353a8d534b6867
Nicolas Mondollot <nmondollot@gmail.com>
2021-02-03 10:52:27 +0100

    Don't use vips

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,6 @@ module Boolder
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    config.active_storage.variant_processor = :vips
+    # config.active_storage.variant_processor = :vips
   end
 end
