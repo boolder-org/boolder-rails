b2d7de69d9c081f57a3c4a8f14428a4504b3d241
Nicolas Mondollot <nmondollot@gmail.com>
2021-03-16 12:37:05 +0100

    Clean up

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -21,6 +21,12 @@ class AreasController < ApplicationController
     @area = Area.find(params[:id])
 
     @parkings = @area.pois
+  end
+
+  def map 
+    @area = Area.find(params[:id])
+
+    @parkings = @area.pois
 
     @annotation = {
       latitude: @parkings.first&.location&.latitude,
@@ -30,42 +36,6 @@ class AreasController < ApplicationController
       glyphText: "",
     } 
 
-    @chart_data = {
-      labels: ['1', '2', '3', '4', '5', '6', '7', '8'],
-      datasets: [{
-          label: 'problems',
-          data: 1.upto(8).map{|i| @area.problems.level(i).count },
-          backgroundColor: 'rgba(5, 150, 105, 0.4)',
-          borderColor: 'rgba(5, 150, 105, 1)',
-          borderWidth: 1
-      }]
-    }
-
-    @chart_options = {
-      scales: {
-          yAxes: [{
-              ticks: {
-                  beginAtZero: true,
-                  max: 150,
-                  stepSize: 50,
-              },
-              # display: false,
-              gridLines: {
-                color: "rgba(200, 200, 200, 0.3)",
-              }
-          }],
-          xAxes: [{
-            gridLines: {
-              display: false,
-            }
-          }]
-      },
-      legend: {
-        display: false
-      },
-      tooltips: {
-        enabled: false
-      },
-    }
+    @hide_nav = true
   end
 end
