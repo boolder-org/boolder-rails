ef2799c48912e5e77c110e5be185c4876857d510
Nicolas Mondollot <nmondollot@gmail.com>
2021-04-05 18:32:50 +0200

    Not null

--- db/migrate/20210405160635_add_risky_to_problems.rb
+++ db/migrate/20210405160635_add_risky_to_problems.rb
@@ -1,5 +1,5 @@
 class AddRiskyToProblems < ActiveRecord::Migration[6.1]
   def change
-    add_column :problems, :risky, :boolean, default: false
+    add_column :problems, :risky, :boolean, default: false, null: false
   end
 end
