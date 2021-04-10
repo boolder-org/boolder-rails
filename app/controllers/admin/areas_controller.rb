bfa69d0faca7f4a91f46f7fb3f97c68454a0b963
Nicolas Mondollot <nmondollot@gmail.com>
2021-04-10 16:08:36 +0200

    Area edit

--- app/controllers/admin/areas_controller.rb
+++ app/controllers/admin/areas_controller.rb
@@ -2,4 +2,22 @@ class Admin::AreasController < Admin::BaseController
   def index
     @areas = Area.order(:name)
   end
+
+  def edit
+    @area = Area.find(params[:id])
+  end
+
+  def update
+    area = Area.find(params[:id])
+    area.update!(area_params)
+
+    flash[:notice] = "Area updated"
+    redirect_to admin_areas_path
+  end
+
+  private 
+  def area_params
+    params.require(:area).
+      permit(:name, :published)
+  end
 end
