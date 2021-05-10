c290e4973489e31e4f2a059cedf0d2c31209c232
Nicolas Mondollot <nmondollot@gmail.com>
2021-05-10 10:47:04 +0200

    Refactor

--- app/helpers/problems_helper.rb
+++ app/helpers/problems_helper.rb
@@ -1,20 +1,22 @@
 module ProblemsHelper
-  def circle_view(problem)
-    content_tag(:span, problem.circuit_number.presence || "&nbsp;".html_safe, 
-      style: "background: #{uicolor(problem.circuit&.color)}; color: #{text_color(problem.circuit&.color)}",
-      class: "rounded-full h-6 w-6 inline-flex leading-6 justify-center")
+  def problem_circle_view(problem)
+    circle_view(problem.circuit_number.presence || "&nbsp;".html_safe, 
+      background_color: uicolor(problem.circuit&.color), 
+      text_color: text_color(problem.circuit&.color)
+    )
   end
 
-  def circle_view_with_name(problem)
-    circle_view(problem) + 
+  def problem_circle_view_with_name(problem)
+    problem_circle_view(problem) + 
       (link_to problem.name_with_fallback, edit_admin_problem_path(problem), class: "ml-2 font-semibold")
   end
 
-  # FIXME: make it DRY
   def circuit_circle_view(circuit, klass: "h-6 w-6 leading-6")
-    content_tag(:span, "&nbsp;".html_safe, 
-      style: "background: #{uicolor(circuit&.color)}; color: #{text_color(circuit&.color)}",
-      class: "rounded-full #{klass} inline-flex justify-center")
+    circle_view("&nbsp;".html_safe, 
+      background_color: uicolor(circuit&.color), 
+      text_color: text_color(circuit&.color),
+      klass: klass
+    )
   end
 
   def uicolor(circuit_color)
@@ -26,6 +28,13 @@ module ProblemsHelper
   end
 
   private
+
+  def circle_view(content, background_color:, text_color:, klass: "h-6 w-6 leading-6")
+    content_tag(:span, content, 
+      style: "background: #{background_color}; color: #{text_color}",
+      class: "rounded-full #{klass} inline-flex justify-center")
+  end
+
   def color_mapping
      {
       yellow:   "#FFCC02",
