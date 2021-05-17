4bdeb60f5c3785c075c6b68eec4284daf4ce2110
Nicolas Mondollot <nmondollot@gmail.com>
2021-05-17 14:27:30 +0200

    Fix crash when http_accept_language is nil

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -17,6 +17,6 @@ class WelcomeController < ApplicationController
 
   private
     def extract_locale_from_accept_language_header
-      request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first
+      request.env['HTTP_ACCEPT_LANGUAGE']&.scan(/^[a-z]{2}/)&.first
     end
 end
