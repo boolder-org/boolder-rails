c757395b2e018bd10d3c6bd921aaf8b82432e82b
Nicolas Mondollot <nmondollot@gmail.com>
2021-06-06 18:58:24 +0200

    Redirect en locale for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,13 @@
 class ArticlesController < ApplicationController
+  before_action :redirect_en
+
   def choose_area
     @beginner_areas = Area.published.reject{|a| a.id == 7}.
       map{|a| [a, a.problems.where("grade < '4a'").count ]}.
       sort_by(&:second).reverse
+  end
+
+  def redirect_en
+    redirect_to request.env['PATH_INFO'].sub('/en', '/fr') if I18n.locale == :en
   end
 end
