43585666834712b50c510b164b8d148872666f92
Nicolas Mondollot <nmondollot@gmail.com>
2021-07-30 18:14:14 +0200

    Upgrade turbo

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,9 +4,9 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
+// import "@hotwired/turbo-rails"
 require("@rails/activestorage").start()
 require("channels")
-// require("@hotwired/turbo-rails")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
