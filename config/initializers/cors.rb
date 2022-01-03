4c1e596898b3fc5cb9b03f9d80182cdde3eb7a56
Nicolas Mondollot <nmondollot@gmail.com>
2022-01-03 11:31:29 +0100

    Restrict cors to boolder.com for cloudfront

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,7 +3,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
-    resource '*', headers: :any, methods: [:get, :post, :patch, :put]
+    origins ['boolder.com', 'www.boolder.com']
+    resource '*', headers: :any, methods: [:get, :head, :options]
   end
 end
