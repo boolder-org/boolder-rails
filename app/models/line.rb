a9f8346abbde33d646d100d9df1ac4da1c954b2b
Nicolas Mondollot <nmondollot@gmail.com>
2022-01-31 09:55:01 +0100

    Line order by created_at

--- app/models/line.rb
+++ app/models/line.rb
@@ -3,4 +3,6 @@ class Line < ApplicationRecord
   belongs_to :topo
 
   scope :published, -> { joins(:topo).where(topos: { published: true }) }
+  default_scope { order(created_at: :asc) } # TODO: use a dedicated order column for multi-topos problems (e.g. traverses)
+
 end
