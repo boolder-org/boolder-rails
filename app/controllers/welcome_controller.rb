f8b367b7bb097c1d5b6bb45f8f09080b33bd5f93
Nicolas Mondollot <nmondollot@gmail.com>
2022-02-06 12:08:34 +0100

    Use shared cache

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ class WelcomeController < ApplicationController
   layout false, only: [:soon]
 
   def index
-    @beginner_areas = Rails.cache.fetch("welcome/index/beginner_friendly_list", expires_in: 12.hours) do
+    @beginner_areas = Rails.cache.fetch("shared/beginner_friendly_list", expires_in: 12.hours) do
       Area.published.any_tags(:beginner_friendly).all.shuffle
     end
   end
