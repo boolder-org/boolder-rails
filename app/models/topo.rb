b71bab395ca3b906e88783099cdb734c9180682a
Nicolas Mondollot <nmondollot@gmail.com>
2022-02-16 22:12:30 +0100

    Shared constant for imgx params for topos

--- app/models/topo.rb
+++ app/models/topo.rb
@@ -3,6 +3,9 @@ class Topo < ApplicationRecord
   has_many :lines, dependent: :destroy
   has_many :problems, through: :lines
 
+  IMGIX_LARGE = { w: 800 }
+  IMGIX_SMALL = { w: 200, dpr: 2, q: 50 }
+
   scope :published, -> { where(published: true) }
 
   validates :photo, presence: true
