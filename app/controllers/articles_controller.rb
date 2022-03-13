571fc04373764447100181a860814446e1bf858b
Nicolas Mondollot <nmondollot@gmail.com>
2022-03-13 11:18:39 +0100

    Translate article "top areas by level"

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,4 @@
 class ArticlesController < ApplicationController
-  before_action :redirect_en
-
   def choose_area
     @beginner_areas = Rails.cache.fetch("shared/beginner_friendly_list", expires_in: 12.hours) do
       Area.published.any_tags(:beginner_friendly).all.shuffle
@@ -46,11 +44,5 @@ class ArticlesController < ApplicationController
         any_tags(:dry_fast).
         shuffle
     end
-  end
-
-  private
-
-  def redirect_en
-    redirect_to request.env['PATH_INFO'].sub('/en', '/fr') if I18n.locale == :en
   end
 end
