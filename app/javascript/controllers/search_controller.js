af7650a4d91c3800c65720da35aa76ae07aa7ed6
Nicolas Mondollot <nmondollot@gmail.com>
2022-10-13 15:50:04 +0200

    Upgrade to stimulus 3

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -1,4 +1,4 @@
-import { Controller } from "stimulus"
+import { Controller } from '@hotwired/stimulus'
 
 import algoliasearch from 'algoliasearch/lite';
 import { autocomplete, getAlgoliaResults } from '@algolia/autocomplete-js';
