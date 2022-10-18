e052e2bc530670c64171a43dc4146f29012a45b3
Nicolas Mondollot <nmondollot@gmail.com>
2022-10-18 19:32:26 +0200

    Map zoom on problem on load

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -2,12 +2,22 @@ class MapController < ApplicationController
   layout "map"
 
   def index
-    if params[:area_id] && (a = Area.find(params[:area_id]))
+    if params[:area_id] && (area = Area.find(params[:area_id]))
       @bounds = { 
-        south_west_lat: a.bounds[:south_west].lat,
-        south_west_lon: a.bounds[:south_west].lon,
-        north_east_lat: a.bounds[:north_east].lat,
-        north_east_lon: a.bounds[:north_east].lon,
+        south_west_lat: area.bounds[:south_west].lat,
+        south_west_lon: area.bounds[:south_west].lon,
+        north_east_lat: area.bounds[:north_east].lat,
+        north_east_lon: area.bounds[:north_east].lon,
+      }
+    end
+
+    if params[:problem_id] && (problem = Problem.find(params[:problem_id]))
+      @problem = { 
+        id: problem.id,
+        lat: problem.location.lat,
+        lon: problem.location.lon,
+        name: problem.name,
+        grade: problem.grade,
       }
     end
   end
