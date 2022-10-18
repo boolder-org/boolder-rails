ced700cd67f1447119437af094ff7e7f235824e0
Nicolas Mondollot <nmondollot@gmail.com>
2022-10-18 18:59:23 +0200

    Map zoom on area on load

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -2,5 +2,13 @@ class MapController < ApplicationController
   layout "map"
 
   def index
+    if params[:area_id] && (a = Area.find(params[:area_id]))
+      @bounds = { 
+        south_west_lat: a.bounds[:south_west].lat,
+        south_west_lon: a.bounds[:south_west].lon,
+        north_east_lat: a.bounds[:north_east].lat,
+        north_east_lon: a.bounds[:north_east].lon,
+      }
+    end
   end
 end
