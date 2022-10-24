81fac9a51b442072739d3ca4c21ac0ab468d99e4
Nicolas Mondollot <nmondollot@gmail.com>
2022-10-24 11:41:58 +0200

    Camelcase

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -9,7 +9,8 @@ class MapController < ApplicationController
         south_west_lon: area.bounds[:south_west].lon,
         north_east_lat: area.bounds[:north_east].lat,
         north_east_lon: area.bounds[:north_east].lon,
-      }
+      }.
+      with_indifferent_access.deep_transform_keys { |key| key.camelize(:lower) }
     end
 
     if params[:pid] && (problem = Problem.find(params[:pid]))
@@ -20,7 +21,8 @@ class MapController < ApplicationController
         name: I18n.with_locale(:fr) { problem.name_with_fallback },
         name_en: I18n.with_locale(:en) { problem.name_with_fallback },
         grade: problem.grade,
-      }.with_indifferent_access.deep_transform_keys { |key| key.camelize(:lower) }
+      }.
+      with_indifferent_access.deep_transform_keys { |key| key.camelize(:lower) }
     end
   end
 end
