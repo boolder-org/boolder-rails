824d71f8fd0c22a2c73eeec857ac3d924d9e19d5
Nicolas Mondollot <nmondollot@gmail.com>
2022-11-01 09:11:48 +0100

    Task for problems with empty bleau_info_id

--- app/controllers/admin/tasks_controller.rb
+++ app/controllers/admin/tasks_controller.rb
@@ -1,7 +1,7 @@
 class Admin::TasksController < Admin::BaseController
   def index
     @area = Area.find_by(slug: params[:area_slug])
-    @tabs = %w(missing_line missing_line_coordinates)
+    @tabs = %w(missing_line missing_line_coordinates empty_bleau_info_id)
 
     if !params[:tab].present?
       redirect_to admin_area_tasks_path(tab: @tabs.first) 
@@ -13,6 +13,8 @@ class Admin::TasksController < Admin::BaseController
     elsif params[:tab] == "missing_line_coordinates"
       @problems = @area.problems.joins("LEFT JOIN lines ON lines.problem_id = problems.id").
         where("lines.coordinates IS NULL AND lines.problem_id IS NOT NULL").uniq
+    elsif params[:tab] == "empty_bleau_info_id"
+      @problems = @area.problems.where("bleau_info_id IS NULL OR bleau_info_id = ''")
     end
   end
 
