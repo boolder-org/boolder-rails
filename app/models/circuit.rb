43d0e08982799185940df7c3e8017d73f60a81e3
Nicolas Mondollot <nmondollot@gmail.com>
2022-11-12 09:06:57 +0100

    Circuit stats rake task

--- app/models/circuit.rb
+++ app/models/circuit.rb
@@ -21,4 +21,55 @@ class Circuit < ApplicationRecord
   def name
       I18n.t("circuit.name.#{color}")
   end
+
+  # FIXME: use most nb of problems
+  def main_area
+  end
+
+  def avg_grade
+    problems_with_grade = problems.select{|p| p.grade.in?(Problem::GRADE_VALUES)}
+    grades_int = problems_with_grade.map{|p| Problem::GRADE_VALUES.index(p.grade) }
+    avg_int = avg(grades_int).round
+    Problem::GRADE_VALUES[avg_int]
+  end
+
+  def median_grade
+    problems_with_grade = problems.select{|p| p.grade.in?(Problem::GRADE_VALUES)}
+    grades_int = problems_with_grade.map{|p| Problem::GRADE_VALUES.index(p.grade) }
+    median_int = median(grades_int).round
+    Problem::GRADE_VALUES[median_int]
+  end
+
+  def avg_height
+    problems_with_height = problems.select{|p| p.height.present? }
+    heights_int = problems_with_height.map{|p| p.height }
+    return 0 if heights_int.count == 0
+    avg(heights_int).round(1)
+  end
+
+  def median_height
+    problems_with_height = problems.select{|p| p.height.present? }
+    heights_int = problems_with_height.map{|p| p.height }
+    return 0 if heights_int.count == 0
+    median(heights_int)
+  end
+
+  def avg_landing_score
+    mapping = { "easy" => 0, "medium" => 1, "hard" => 2 }
+    problems_with_landing = problems.select{|p| p.landing.present? }
+    landing_scores = problems_with_landing.map{|p| mapping[p.landing] }
+    return 0 if landing_scores.count == 0
+    avg(landing_scores).round(3)
+  end
+
+  def avg(array)
+    array.reduce(:+) / array.size.to_f
+  end
+
+  def median(array)
+    return nil if array.empty?
+    sorted = array.sort
+    len = sorted.length
+    (sorted[(len - 1) / 2] + sorted[len / 2]) / 2.0
+  end
 end
---
+++ lib/tasks/circuit.rake
@@ -0,0 +1,28 @@
+require 'csv'
+
+namespace :circuit do
+
+  task stats: :environment do
+    file_name = Rails.root.join('export', "circuits.csv")
+    CSV.open(file_name, "w") do |csv|
+
+      csv << %w(id color area avg_grade median_grade avg_height median_height avg_landing_score)
+
+      Circuit.all.each do |c|  
+        puts c.id
+        csv << [
+          c.id,
+          c.color,
+          c.areas.first&.name,
+          c.avg_grade,
+          c.median_grade,
+          c.avg_height,
+          c.median_height,
+          c.avg_landing_score
+        ]
+      end
+
+      puts "exported circuits.csv".green
+    end
+  end
+end
