eba494d8e44aca11849148c817e0c3298aad914e
Nicolas Mondollot <nmondollot@gmail.com>
2022-12-07 17:07:32 +0100

    Popular areas on homepage

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,7 @@ class WelcomeController < ApplicationController
 
   def index
     @beginner_areas = Rails.cache.fetch("shared/beginner_friendly_list", expires_in: 12.hours) do
-      Area.published.any_tags(:beginner_friendly).all.shuffle
+      Area.published.any_tags(:popular).all.shuffle
     end
   end
 
