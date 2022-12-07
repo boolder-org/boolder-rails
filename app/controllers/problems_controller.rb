d3d618638dbb4c5f2f13330318e04d9cea159f35
Nicolas Mondollot <nmondollot@gmail.com>
2022-12-07 17:07:15 +0100

    Tweaks

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -8,14 +8,14 @@ class ProblemsController < ApplicationController
     @grades = %w(3a 3b 3c 4a 4b 4c 5a 5b 5c 6a 6b 6c 7a 7b 7c 8a 8b 8c 9a)
 
     if params[:grade].in?(@grades) 
-      grade = params[:grade]
+      @grade = params[:grade]
     else
-      grade = "6a"
+      @grade = "6a"
     end
 
     @problems = Problem.joins(:area).where(area: { published: true }).
       where("rating > 0").
-      where(grade: [grade, "#{grade}+"]).
+      where(grade: [@grade, "#{@grade}+"]).
       order(rating: :desc).limit(2000)
       # where("rating > 50").where(grade: ["6a", "6a+"]).order(grade: :desc)
   end
