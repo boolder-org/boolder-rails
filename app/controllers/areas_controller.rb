9edd5d53b4eae9a3d2847df6c9ecb57242f5dc91
Nicolas Mondollot <nmondollot@gmail.com>
2022-12-12 19:08:17 +0100

    Clean up

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -2,27 +2,13 @@ class AreasController < ApplicationController
   def index
     @areas = Area.published
 
-    if params[:sort] == "name"
-      @areas_with_count = @areas.map {|area| [area, area.problems.count]}
-      @areas_with_count = @areas_with_count.sort{|a,b| ActiveSupport::Inflector.transliterate(a.first.name) <=> ActiveSupport::Inflector.transliterate(b.first.name) }
-    elsif params[:sort].to_i.in?(1..8)
-      @areas_with_count = @areas.map {|area| [area, area.problems.level(params[:sort].to_i).count]}
-      @areas_with_count = @areas_with_count.sort{|a,b| b.second <=> a.second }
-    else
-      @areas_with_count = @areas.map {|area| [area, area.problems.count]}
-      @areas_with_count = @areas_with_count.sort{|a,b| b.second <=> a.second }
-    end
+    @areas_with_count = @areas.map {|area| [area, area.problems.count]}
+    @areas_with_count = @areas_with_count.sort{|a,b| b.second <=> a.second }
   end
 
   def show
     @area = Area.find_by(slug: params[:slug])
 
-    @grades = @area.problems.order(popularity: :desc).group_by{|p| p.grade }.sort_by{|grade, _| grade }.reverse
-  end
-
-  def problems
-    @area = Area.find_by(slug: params[:slug])
-    
     @grades = @area.problems.order(popularity: :desc).group_by{|p| p.grade }.sort_by{|grade, _| grade }.reverse
   end
 end
