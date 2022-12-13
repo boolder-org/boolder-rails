a0d20b543778aaea270c0d3cfba9273b4e490e5c
Nicolas Mondollot <nmondollot@gmail.com>
2022-12-13 18:23:15 +0100

    Refactor

--- app/helpers/areas_helper.rb
+++ app/helpers/areas_helper.rb
@@ -6,8 +6,8 @@ module AreasHelper
     )
   end
 
-  def level_bar(level: "", disabled: false, klass: "h-6 w-6 leading-6")
-    background_color = disabled ? "rgba(204, 204, 204, .5)" : "rgba(5, 150, 105, 0.8)"
+  def level_bar(level: "", enabled: true, klass: "h-6 w-6 leading-6")
+    background_color = enabled ? "rgba(5, 150, 105, 0.8)" : "rgba(204, 204, 204, .5)"
 
     content_tag(:span, level, 
       style: "background-color: #{background_color}; color: #FFF",
