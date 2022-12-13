951b1ea2ee0fee46b5bb9baec000a95fc535d3cd
Nicolas Mondollot <nmondollot@gmail.com>
2022-12-13 15:14:38 +0100

    Clean up

--- app/controllers/discover_controller.rb
+++
@@ -1,9 +0,0 @@
-class DiscoverController < ApplicationController
-  def index
-    @popular_areas = Area.published.any_tags(:popular).all.sort_by{|a| -a.problems.count }
-
-    @problems = Problem.joins(:area).where(area: { published: true }).where("rating > 50").where(grade: ["6a", "6a+"]).order(grade: :desc)
-
-    @grouped = @problems.group_by{|p| p.area }.sort_by{|k,v| -v.count }
-  end
-end
