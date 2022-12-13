0fe5cfd43610be5215dcd3a29c8baf23bca41d64
Nicolas Mondollot <nmondollot@gmail.com>
2022-12-13 15:27:25 +0100

    Rename avg

--- app/controllers/circuits_controller.rb
+++ app/controllers/circuits_controller.rb
@@ -1,6 +1,6 @@
 class CircuitsController < ApplicationController
   def index
-    @circuits = Circuit.all.sort_by{|c| c.avg_grade }
+    @circuits = Circuit.all.sort_by{|c| c.average_grade }
   end
 
   def show
