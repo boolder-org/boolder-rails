388e908c4d1a9eae3f549882c66a773f14502780
Nicolas Mondollot <nmondollot@gmail.com>
2022-12-13 18:36:48 +0100

    Refactor

--- app/helpers/areas_helper.rb
+++ app/helpers/areas_helper.rb
@@ -5,13 +5,4 @@ module AreasHelper
       locals: { title: title, color: color, block: block }
     )
   end
-
-  def level_bar(level: "", enabled: true, klass: "h-6 w-6 leading-6")
-    background_color = enabled ? "rgba(5, 150, 105, 0.8)" : "rgba(204, 204, 204, .5)"
-
-    content_tag(:span, level, 
-      style: "background-color: #{background_color}; color: #FFF",
-      class: "rounded-sm text-sm #{klass} inline-flex justify-center"
-    )
-  end
 end
