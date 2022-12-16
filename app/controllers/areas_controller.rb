e423e6e4d3bb9dbf506b645d2008ceb641bfb3a7
Nicolas Mondollot <nmondollot@gmail.com>
2022-12-16 09:31:36 +0100

    Turbo frame to improve perfs

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -7,6 +7,11 @@ class AreasController < ApplicationController
     @area = Area.find_by(slug: params[:slug])
 
     @circuits = @area.main_circuits
+  end
+
+  def problems
+    @area = Area.find_by(slug: params[:slug])
+    
     @problems = @area.problems.order(popularity: :desc).group_by{|p| p.grade }.sort_by{|grade, _| grade }.reverse
   end
 end
