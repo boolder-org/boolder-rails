fe21722e64978ab655235f3326d9981ca47c5cd8
Nicolas Mondollot <nmondollot@gmail.com>
2023-01-08 17:27:34 +0100

    PoiRoute distance in minutes

--- app/models/poi_route.rb
+++ app/models/poi_route.rb
@@ -2,11 +2,30 @@ class PoiRoute < ApplicationRecord
   belongs_to :area
   belongs_to :poi
 
+  # TRANSPORT_VALUES = %w(walking bike)
+
   def distance_in_minutes
-    (distance.to_f / 80).round
+    if transport.to_s == "bike"
+      friendly_round(distance.to_f / 240)
+    else
+      friendly_round(distance.to_f / 80)
+    end
   end
 
   def description
     "#{poi.name} (#{distance_in_minutes} min)"
+  end
+
+  private
+  def friendly_round(n)
+    if n <= 2
+      2
+    else
+      round_up(n.round, 5)
+    end
+  end
+
+  def round_up(n, increment)
+    increment * (( n + increment - 1) / increment)
   end
 end
