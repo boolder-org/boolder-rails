675a41d864768d6f75d876763721a1ac58099c73
Nicolas Mondollot <nmondollot@gmail.com>
2023-01-12 13:48:35 +0100

    Remove circuits.order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_12_124430) do
+ActiveRecord::Schema.define(version: 2023_01_12_124744) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,7 +74,6 @@ ActiveRecord::Schema.define(version: 2023_01_12_124430) do
     t.string "color"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "order"
     t.integer "risk", limit: 2
   end
 
