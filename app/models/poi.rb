110094a512299d147cd808dd9cda943bf9e899cd
Nicolas Mondollot <nmondollot@gmail.com>
2023-01-12 20:30:56 +0100

    Poi routes on area show

--- app/models/poi.rb
+++ app/models/poi.rb
@@ -1,3 +1,15 @@
 class Poi < ApplicationRecord
   # has_many :areas
+
+  TYPE_VALUES = %w(parking train_station)
+
+  validates :poi_type, inclusion: { in: TYPE_VALUES }
+
+  def parking?
+    poi_type.to_s == "parking"
+  end
+
+  def train_station?
+    poi_type.to_s == "train_station"
+  end
 end
