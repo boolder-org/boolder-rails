d5af2289e498dcaa7a4df7261381ab118fd52dea
Nicolas Mondollot <nmondollot@gmail.com>
2023-04-30 10:40:02 +0200

    Link to backoffice

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,4 +1,9 @@
 class Admin::BaseController < ApplicationController
   http_basic_authenticate_with name: "nico", password: Rails.application.credentials.dig(:admin, :http_password)
   layout "admin"
+  before_action :set_cookie
+
+  def set_cookie
+    session[:admin] = true
+  end
 end
