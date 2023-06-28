f1a9dacb822d238d13f152181e212064714c6fd9
Nicolas Mondollot <nmondollot@gmail.com>
2023-06-28 10:47:55 +0200

    Rails 7.0 defaults

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module Boolder
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
     #
@@ -18,21 +18,5 @@ module Boolder
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
-
-    # REMOVE THE LINES BELOW WHEN THE SWITCH TO LOAD DEFAULTS 7.0 IS DONE 
-
-    # Disables the deprecated #to_s override in some Ruby core classes
-    # See https://guides.rubyonrails.org/configuring.html#config-active-support-disable-to-s-conversion for more information.
-    config.active_support.disable_to_s_conversion = true
-
-    # ** Please read carefully, this must be configured in config/application.rb **
-    # Change the format of the cache entry.
-    # Changing this default means that all new cache entries added to the cache
-    # will have a different format that is not supported by Rails 6.1 applications.
-    # Only change this value after your application is fully deployed to Rails 7.0
-    # and you have no plans to rollback.
-    # When you're ready to change format, add this to `config/application.rb` (NOT this file):
-     config.active_support.cache_format_version = 7.0
   end
 end
