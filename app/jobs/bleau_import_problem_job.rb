eecc36479d10012811bb9a844dc28e284333b993
Nicolas Mondollot <nmondollot@gmail.com>
2023-07-04 16:02:28 +0200

    Steepness and grade

--- app/jobs/bleau_import_problem_job.rb
+++ app/jobs/bleau_import_problem_job.rb
@@ -8,10 +8,26 @@ class BleauImportProblemJob < ApplicationJob
     doc = Nokogiri::HTML(html)
 
     name = doc.css("h3")[0].text.split("\n").second.strip
-    grade = doc.css("h3 em")[0].text.split("\n").second.strip
+    grade_bleau = doc.css("h3 em")[0].text.split("\n").second.strip
+    grade = BLEAU_INFO_GRADE_MAPPING[grade_bleau] || grade_bleau
+    return if grade == "P" # P is for Project => no official grade yet
 
-    tags = doc.css(".btype").first.text.strip
+    tags = doc.css(".btype").first.text.strip.split(", ")
     sit_start = tags.include?("sitstart")
+
+    steepness = if tags.include?("traverse") || tags.include?("traverse fltr") || tags.include?("traverse frtl")
+      :traverse
+    elsif tags.include?("roof")
+      :roof
+    elsif tags.include?("slightly overhanging") || tags.include?("overhang")
+      :overhang
+    elsif tags.include?("slab")
+      :slab
+    elsif tags.include?("wall")
+      :wall
+    else
+      :other
+    end
 
     circuit = doc.css('.bcircuit a').select { |link| link['href'].include?(".html") }.first
     circuit_number_and_letter = circuit&.text&.split(":")&.second&.strip
@@ -37,6 +53,8 @@ class BleauImportProblemJob < ApplicationJob
       name: name,
       grade: grade,
       sit_start: sit_start,
+      steepness: steepness,
+      tags: tags,
       bleau_circuit_id: bleau_circuit_id,
       circuit_number: circuit_number,
       circuit_letter: circuit_letter,
@@ -52,3 +70,21 @@ class BleauImportProblemJob < ApplicationJob
     Rails.logger.debug "Updated bleau_problem ##{id}"
   end
 end
+
+BLEAU_INFO_GRADE_MAPPING = {
+  "1-" => "1a",
+  "1"  => "1b",
+  "1+" => "1c",
+  "2-" => "2a",
+  "2"  => "2b",
+  "2+" => "2c",
+  "3-" => "3a",
+  "3"  => "3b",
+  "3+" => "3c",
+  "4-" => "4a",
+  "4"  => "4b",
+  "4+" => "4c",
+  "5-" => "5a",
+  "5"  => "5b",
+  "5+" => "5c",
+}
--- app/models/bleau_problem.rb
+++ app/models/bleau_problem.rb
@@ -1,5 +1,16 @@
 class BleauProblem < ApplicationRecord
   belongs_to :bleau_area
 
-  # concern for grade
+  # TODO: make DRY with problem.rb
+  validates :steepness, inclusion: { in: STEEPNESS_VALUES }
+  validates :grade, inclusion: { in: Problem::GRADE_VALUES }, allow_blank: true
+  validates :circuit_letter, inclusion: { in: Problem::LETTERS.keys }, allow_blank: true
+  validate :validate_circuit_letter_is_numeric
+
+  def validate_circuit_letter_is_numeric
+    return if circuit_number.blank? || circuit_number == LETTER_START
+    if circuit_number.to_i < 1
+      errors.add(:circuit_number, "must be a number or D (for Départ)")
+    end
+  end
 end
