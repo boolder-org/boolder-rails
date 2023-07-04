3a9097042d5d64ba1409d7f6b49fa07c9e031e12
Nicolas Mondollot <nmondollot@gmail.com>
2023-07-04 16:17:54 +0200

    Whoops

--- app/models/bleau_problem.rb
+++ app/models/bleau_problem.rb
@@ -2,7 +2,7 @@ class BleauProblem < ApplicationRecord
   belongs_to :bleau_area
 
   # TODO: make DRY with problem.rb
-  validates :steepness, inclusion: { in: STEEPNESS_VALUES }
+  validates :steepness, inclusion: { in: Problem::STEEPNESS_VALUES }
   validates :grade, inclusion: { in: Problem::GRADE_VALUES }, allow_blank: true
   validates :circuit_letter, inclusion: { in: Problem::LETTERS.keys }, allow_blank: true
   validate :validate_circuit_letter_is_numeric
