fb86b1d1a5bd51f48b4948be224bf65d461ebb74
Nicolas Mondollot <nmondollot@gmail.com>
2023-09-27 11:16:29 +0200

    WIP

--- app/controllers/admin/tasks_controller.rb
+++ app/controllers/admin/tasks_controller.rb
@@ -2,18 +2,25 @@ class Admin::TasksController < Admin::BaseController
   def index
     @area = Area.find_by(slug: params[:area_slug])
     
-    @todos = Todo.joins(:problem).where(problems: { area_id: @area.id }).order("ascents DESC NULLS LAST")
+    @todos = Todo.joins(:problem).
+      where(problems: { area_id: @area.id }).
+      where.not(todos: { reason: "line" }).
+      order("ascents DESC NULLS LAST")
 
     @bleau_problems = BleauProblem.
       left_outer_joins(:problem).where(problems: { id: nil }).
-      where(bleau_area_id: BleauArea.find_by(slug: "isatis").id).
+      where(bleau_area_id: BleauArea.find_by(slug: "cuvier").id).
       # where.not(ascents: nil).
       order("ascents DESC NULLS LAST")
   end
 
   def dashboard
-    @areas_with_ascents = Todo.all.joins(:problem).group(:area_id).sum(:ascents).
-      sort_by(&:second).reverse.
-      select{|area_id, _| Area.find(area_id).published }
+    @areas_with_ascents = Area.published.
+      map{|a| [a, 1 - a.problems.joins(:todos).where.not(todos: { reason: "line" }).sum(:ascents).to_f / a.problems.sum(:ascents).to_f] }.
+      sort_by(&:second)
+
+    # @areas_with_ascents = Todo.all.joins(:problem).group(:area_id).sum(:ascents).
+    #   sort_by(&:second).reverse.
+    #   select{|area_id, _| Area.find(area_id).published }
   end
 end
