82ca7235aa24d47a2353a46df5e82a6cda5a14ec
Nicolas Mondollot <nmondollot@gmail.com>
2023-10-05 19:24:53 +0200

    Clean up

--- app/controllers/admin/bleau_problems_controller.rb
+++ app/controllers/admin/bleau_problems_controller.rb
@@ -1,11 +1,8 @@
 class Admin::BleauProblemsController < Admin::BaseController
   def index
-    # @area = Area.find_by(slug: params[:area_slug])
-    @area = Area.find(22)
-
     @bleau_problems = BleauProblem.
       left_outer_joins(:problem).where(problems: { id: nil }).
-      # where(bleau_area_id: @area.bleau_area_id).
+      # where(bleau_area_id: Area.find(4).bleau_area_id).
       # where.not(ascents: nil).
       order("ascents DESC NULLS LAST")
   end
