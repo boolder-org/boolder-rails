426cb2c60c32180bcfbf96ce73945eeb5df66e0d
Nicolas Mondollot <nmondollot@gmail.com>
2023-10-05 15:04:56 +0200

    Contribution requests index

--- app/controllers/contribute/contribution_requests_controller.rb
+++ app/controllers/contribute/contribution_requests_controller.rb
@@ -1,16 +1,10 @@
 class Contribute::ContributionRequestsController < ApplicationController
   def index
-    @area = Area.find_by(slug: params[:area_slug])
+    @area = Area.find_by(slug: "cuvier")
     
-    @todos = ContributionRequest.joins(:problem).
+    @requests = ContributionRequest.joins(:problem).
       where(problems: { area_id: @area.id }).
-      # where.not(todos: { reason: "line" }).
-      order("ascents DESC NULLS LAST")
-
-    @bleau_problems = BleauProblem.
-      left_outer_joins(:problem).where(problems: { id: nil }).
-      where(bleau_area_id: BleauArea.find_by(slug: "cuvier").id).
-      # where.not(ascents: nil).
+      where(what: "photo").
       order("ascents DESC NULLS LAST")
   end
 
@@ -18,9 +12,5 @@ class Contribute::ContributionRequestsController < ApplicationController
     @areas = Area.published.
       map{|a| [a, 1 - a.problems.joins(:contribution_requests).where(contribution_requests: { what: "photo" }).sum(:ascents).to_f / a.problems.sum(:ascents).to_f] }.
       sort_by(&:second)
-
-    # @areas_with_ascents = ContributionRequest.all.joins(:problem).group(:area_id).sum(:ascents).
-    #   sort_by(&:second).reverse.
-    #   select{|area_id, _| Area.find(area_id).published }
   end
 end
