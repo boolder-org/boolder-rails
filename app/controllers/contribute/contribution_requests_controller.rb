a55388502e3d044db3d4653a62394bd67a60db0d
Nicolas Mondollot <nmondollot@gmail.com>
2023-10-07 22:42:11 +0200

    Drawer for problems with less than 5 ascents

--- app/controllers/contribute/contribution_requests_controller.rb
+++ app/controllers/contribute/contribution_requests_controller.rb
@@ -1,11 +1,19 @@
 class Contribute::ContributionRequestsController < ApplicationController
+  MIN_ASCENTS = 5
+
   def index
     @area = Area.find(params[:area_id])
     
     @requests = ContributionRequest.joins(:problem).
       where(problems: { area_id: @area.id }).
       where(what: "photo").
-      where("ascents >= ?", 5).
+      where("ascents >= ?", MIN_ASCENTS).
+      order("ascents DESC NULLS LAST")
+
+    @requests_rest = ContributionRequest.joins(:problem).
+      where(problems: { area_id: @area.id }).
+      where(what: "photo").
+      where("ascents < ?", MIN_ASCENTS).
       order("ascents DESC NULLS LAST")
   end
 
@@ -13,7 +21,7 @@ class Contribute::ContributionRequestsController < ApplicationController
     @areas = Area.published.
       map{|a| [
           a, 
-          a.problems.joins(:contribution_requests).where(contribution_requests: { what: "photo" }).where("ascents >= ?", 5).count,
+          a.problems.joins(:contribution_requests).where(contribution_requests: { what: "photo" }).where("ascents >= ?", MIN_ASCENTS).count,
           1 - a.problems.joins(:contribution_requests).where(contribution_requests: { what: "photo" }).sum(:ascents).to_f / a.problems.sum(:ascents).to_f
         ]
       }.
