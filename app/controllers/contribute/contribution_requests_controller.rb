66f653a5400b2061bced3d163800e308542ef357
Nicolas Mondollot <nmondollot@gmail.com>
2023-10-07 22:30:42 +0200

    Simplify UI

--- app/controllers/contribute/contribution_requests_controller.rb
+++ app/controllers/contribute/contribution_requests_controller.rb
@@ -5,12 +5,18 @@ class Contribute::ContributionRequestsController < ApplicationController
     @requests = ContributionRequest.joins(:problem).
       where(problems: { area_id: @area.id }).
       where(what: "photo").
+      where("ascents >= ?", 5).
       order("ascents DESC NULLS LAST")
   end
 
   def dashboard
     @areas = Area.published.
-      map{|a| [a, 1 - a.problems.joins(:contribution_requests).where(contribution_requests: { what: "photo" }).sum(:ascents).to_f / a.problems.sum(:ascents).to_f] }.
-      sort_by(&:second)
+      map{|a| [
+          a, 
+          a.problems.joins(:contribution_requests).where(contribution_requests: { what: "photo" }).where("ascents >= ?", 5).count,
+          1 - a.problems.joins(:contribution_requests).where(contribution_requests: { what: "photo" }).sum(:ascents).to_f / a.problems.sum(:ascents).to_f
+        ]
+      }.
+      sort_by(&:second).reverse
   end
 end
