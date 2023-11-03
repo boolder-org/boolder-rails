9a75d0b914aadc2f5b1e7d9aca0eb7af52d70624
Nicolas Mondollot <nmondollot@gmail.com>
2023-11-03 19:43:07 +0100

    Change colors

--- app/helpers/admin/moderation/problems_helper.rb
+++ app/helpers/admin/moderation/problems_helper.rb
@@ -1,13 +1,11 @@
 module Admin::Moderation::ProblemsHelper
   def percentage_color(percentage)
-    if percentage < 0.8
-      "#FF0000" # red
-    elsif percentage < 0.90
-      "#F7BA00" # yellow
-    elsif percentage < 0.95
-      "#FF9400" # orange
-    else
+    if percentage >= 0.95
       "#1EB100" # green
+    elsif percentage >= 0.85
+      "#F7BA00" # yellow
+    else
+      "#FF0000" # red
     end
   end
 end
