609c184ad8b8607cc6e591ce7c6db504b53336fa
Nicolas Mondollot <nmondollot@gmail.com>
2023-11-06 14:26:46 +0100

    lol

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,7 +2,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
-module.exports = = async function (env, argv) {
+module.exports = async function (env, argv) {
   const config = await environment.toWebpackConfig()
 
   // Customize the config before returning it.
