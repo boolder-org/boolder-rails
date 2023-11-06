d63af9e912e41d2b381375ab15da16214398e4a6
Nicolas Mondollot <nmondollot@gmail.com>
2023-11-06 14:37:31 +0100

    seriously

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,12 +1,8 @@
+process.env.NODE_OPTIONS = '--openssl-legacy-provider'
+
 process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
-module.exports = async function (env, argv) {
-  const config = await environment.toWebpackConfig()
 
-  // Customize the config before returning it.
-  config.output.hashFunction = require('xxhash-addon').XXHash64;
-  return config;
-};
 
