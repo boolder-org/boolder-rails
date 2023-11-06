19622ae219d3f7897e2d06e1609ffeaf36e4a642
Nicolas Mondollot <nmondollot@gmail.com>
2023-11-06 14:26:15 +0100

    I don't know what I'm doing

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,6 +2,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
-module.exports = environment.toWebpackConfig()
+module.exports = = async function (env, argv) {
+  const config = await environment.toWebpackConfig()
 
-output.hashFunction = 'xxhash64'
+  // Customize the config before returning it.
+  config.output.hashFunction = 'xxhash64';
+  return config;
+};
+
