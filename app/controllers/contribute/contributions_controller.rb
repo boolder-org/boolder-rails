0650437ff3d86d2e0806b9e6120d72a560b51318
Nicolas Mondollot <nmondollot@gmail.com>
2023-11-09 16:23:41 +0100

    Handle form errors

--- app/controllers/contribute/contributions_controller.rb
+++ app/controllers/contribute/contributions_controller.rb
@@ -5,14 +5,20 @@ class Contribute::ContributionsController < Contribute::BaseController
 
   def new
     @contribution = Contribution.new
-
-    @problem = Problem.find(params[:problem_id])
+    @contribution.problem_id = params[:problem_id] 
   end
 
   def create
-    contribution = Contribution.create!(contribution_params)
-    flash[:notice] = "Contribution created!"
-    redirect_to [:contribute, contribution.problem]
+    @contribution = Contribution.new(contribution_params)
+    # @contribution.location = 
+
+    if @contribution.save
+      flash[:notice] = "Contribution created!"
+      redirect_to [:contribute, @contribution.problem]
+    else
+      # flash[:error] = "Error"
+      render "new", status: :unprocessable_entity
+    end
   end
 
   private
