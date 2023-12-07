6c323793ae9a9667c966bccb7883c6cb83e3e9c4
Nicolas Mondollot <nmondollot@gmail.com>
2023-12-07 19:28:07 +0100

    DRY code

--- app/controllers/admin/contribution_requests_controller.rb
+++ app/controllers/admin/contribution_requests_controller.rb
@@ -7,22 +7,16 @@ class Admin::ContributionRequestsController < Admin::BaseController
   def new 
     @problem = Problem.find(params[:problem_id])
 
-    location = if session[:last_location_estimated_lat].present? && session[:last_location_estimated_lon].present?
-      "POINT(#{session[:last_location_estimated_lon]} #{session[:last_location_estimated_lat]})"
-    end
-
-    @contribution_request = @problem.contribution_requests.build(location_estimated: location)
+    @contribution_request = @problem.contribution_requests.build(location_estimated: location_estimated_in_session)
   end
 
   def create
     contribution_request = ContributionRequest.new(what: "photo", state: "open")
     contribution_request.assign_attributes(contribution_request_params)
 
-    # TODO: make DRY 
-    session[:last_location_estimated_lat] = params[:contribution_request][:location_estimated_lat]
-    session[:last_location_estimated_lon] = params[:contribution_request][:location_estimated_lon]
-
     contribution_request.save!
+
+    save_location_estimated_in_session(contribution_request.location_estimated)
 
     flash[:notice] = "Contribution Request created"
     redirect_to [:admin, contribution_request.problem]
@@ -31,8 +25,7 @@ class Admin::ContributionRequestsController < Admin::BaseController
   def edit
     @contribution_request = ContributionRequest.find(params[:id])
 
-    session[:last_location_estimated_lat] = @contribution_request.location_estimated.lat
-    session[:last_location_estimated_lon] = @contribution_request.location_estimated.lon
+    save_location_estimated_in_session(@contribution_request.location_estimated)
   end
 
   def update
@@ -40,15 +33,9 @@ class Admin::ContributionRequestsController < Admin::BaseController
 
     @contribution_request.assign_attributes(contribution_request_params)
 
-    # TODO: make DRY
-    lat = params[:contribution_request][:location_estimated_lat]
-    lon = params[:contribution_request][:location_estimated_lon]
-
-    if lat.present? && lon.present?
-      @contribution_request.location_estimated = "POINT(#{lon} #{lat})"      
-    end
-
     @contribution_request.save!
+
+    save_location_estimated_in_session(@contribution_request.location_estimated)
 
     flash[:notice] = "Contribution request updated"
     redirect_to [:admin, @contribution_request.problem]
@@ -58,5 +45,16 @@ class Admin::ContributionRequestsController < Admin::BaseController
   def contribution_request_params
     params.require(:contribution_request).
       permit(:problem_id, :location_estimated_lat, :location_estimated_lon)
+  end
+
+  def save_location_estimated_in_session(location)
+    session[:last_location_estimated_lat] = location.lat
+    session[:last_location_estimated_lon] = location.lon
+  end
+
+  def location_estimated_in_session
+    if session[:last_location_estimated_lat].present? && session[:last_location_estimated_lon].present?
+      "POINT(#{session[:last_location_estimated_lon]} #{session[:last_location_estimated_lat]})"
+    end
   end
 end
