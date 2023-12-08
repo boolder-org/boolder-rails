571c0d7a6618ec0cf1edb3ba0d6c27d5653c5299
Nicolas Mondollot <nmondollot@gmail.com>
2023-12-08 10:08:16 +0100

    Doc

--- app/models/concerns/geolocatable.rb
+++ app/models/concerns/geolocatable.rb
@@ -1,6 +1,8 @@
 module Geolocatable
   extend ActiveSupport::Concern
 
+  # This adds convenience methods to build a POINT object for `column_name` directly
+  # from `column_name_lat` and `column_name_lon` passed by parameters (eg. in a form)
   class_methods do
     def geolocatable(column_name)
       attr_accessor "#{column_name}_lat", "#{column_name}_lon"
