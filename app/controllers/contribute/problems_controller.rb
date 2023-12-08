85d9d18a9df4db9b14a09f2ce4d98fff08a0d1b6
Nicolas Mondollot <nmondollot@gmail.com>
2023-12-08 10:03:41 +0100

    Clean

--- app/controllers/contribute/problems_controller.rb
+++ app/controllers/contribute/problems_controller.rb
@@ -1,7 +1,5 @@
 class Contribute::ProblemsController < Contribute::BaseController
   def show
     @problem = Problem.find(params[:id])
-
-    @main_contribution_request = @problem.contribution_requests.first # TODO: decide which one to use
   end
 end
