de9b2e4ca0ebc27b11f312273b778d59526658ae
Nicolas Mondollot <nmondollot@gmail.com>
2023-12-08 10:28:59 +0100

    Remove contribute nav

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   around_action :switch_locale
   before_action :set_alternate_tags
-  helper_method :nav_style
 
   def default_url_options
     { locale: I18n.locale }
@@ -18,10 +17,5 @@ class ApplicationController < ActionController::Base
       "en" => request.original_url.sub("/#{I18n.locale}", '/en'),
       # "x-default" => root_url(locale: nil),
     }
-  end
-
-  protected
-  def nav_style
-    :default
   end
 end
--- app/controllers/contribute/base_controller.rb
+++ app/controllers/contribute/base_controller.rb
@@ -1,6 +1,2 @@
 class Contribute::BaseController < ApplicationController
-  # private
-  # def nav_style
-  #   :contribute
-  # end
 end
