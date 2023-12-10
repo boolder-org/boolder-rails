05b8f938af0c516dae58314b046ae5878c597f3c
Nicolas Mondollot <nmondollot@gmail.com>
2023-12-10 22:01:25 +0100

    Dev config for mailers

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,8 +36,10 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-  # Don't care if the mailer can't send.
+  # email
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :file
+  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
 
   config.action_mailer.perform_caching = false
 
