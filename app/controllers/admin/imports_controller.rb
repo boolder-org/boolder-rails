c3c0697622637267beaa16d8b234f685265d3014
Nicolas Mondollot <nmondollot@gmail.com>
2024-01-12 10:02:04 +0100

    Move logic to model

--- app/controllers/admin/imports_controller.rb
+++ app/controllers/admin/imports_controller.rb
@@ -15,65 +15,7 @@ class Admin::ImportsController < Admin::BaseController
   end
 
   def show
-    # area_id = params[:import][:area_id]
-    # raise "please choose an area_id" unless area_id
-
-    # save = (params[:commit] == "Import")
-
-    area_id = 1
-
     @import = Import.find(params[:id])
-
-    data = RGeo::GeoJSON.decode(@import.file.download)
-    problem_features = data.select{|f| f.geometry.is_a?(RGeo::Geos::CAPIPointImpl) }
-    boulder_features = data.select{|f| f.geometry.is_a?(RGeo::Geos::CAPILineStringImpl) }
-
-    @objects = []
-
-    ActiveRecord::Base.transaction do
-      problem_features.each do |feature|
-
-        if feature["problemId"].present?
-          problem = Problem.find_by(id: feature["problemId"])
-          # raise "wrong area for problem #{problem.id}: #{problem.area_id} instead of #{area_id}" if (problem.area_id != area_id.to_i)
-        else
-          problem = Problem.new
-        end
-
-        # TODO: raise if problemId is not present but other attribute is present
-        # it might be a mistake when I created the point in josm
-
-        problem.assign_attributes(
-          area_id: area_id,
-          location: FACTORY.point(feature.geometry.x, feature.geometry.y),
-        )
-
-        # problem.save! if save
-        @objects << problem
-      end
-
-      boulder_features.each do |feature|
-        polygon = FACTORY.polygon(feature.geometry)
-
-        if feature["boulderId"].present?
-          boulder = Boulder.find_by(id: feature["boulderId"])
-          # raise "wrong area for boulder #{boulder.id}: #{boulder.area_id} instead of #{area_id}" if (boulder.area_id != area_id.to_i)
-        else
-          if existing_boulder = Boulder.where(polygon: polygon).first
-            raise "boulder already exists (boulder_id=#{existing_boulder.id})" 
-          else
-            boulder = Boulder.new(area_id: area_id)
-          end
-        end
-
-        boulder.assign_attributes(
-          polygon: polygon
-        )
-
-        # boulder.save! if save
-        @objects << boulder
-      end
-    end
   end
 
   private
--- app/models/import.rb
+++ app/models/import.rb
@@ -1,3 +1,52 @@
 class Import < ApplicationRecord
   has_one_attached :file
+
+  def changes
+    data = RGeo::GeoJSON.decode(file.download)
+    problem_features = data.select{|f| f.geometry.is_a?(RGeo::Geos::CAPIPointImpl) }
+    boulder_features = data.select{|f| f.geometry.is_a?(RGeo::Geos::CAPILineStringImpl) }
+
+    objects = []
+
+    area_id = 1 # FIXME
+
+    problem_features.each do |feature|
+      if feature["problemId"].present?
+        problem = Problem.find_by(id: feature["problemId"])
+        # raise "wrong area for problem #{problem.id}: #{problem.area_id} instead of #{area_id}" if (problem.area_id != area_id.to_i)
+      else
+        problem = Problem.new(area_id: area_id)
+      end
+
+      # TODO: raise if problemId is not present but other attribute is present
+      # it might be a mistake when I created the point in josm
+
+      problem.assign_attributes(
+        location: FACTORY.point(feature.geometry.x, feature.geometry.y)
+      )
+
+      objects << problem
+    end
+
+    boulder_features.each do |feature|
+      polygon = FACTORY.polygon(feature.geometry)
+      if feature["boulderId"].present?
+        boulder = Boulder.find_by(id: feature["boulderId"])
+        # raise "wrong area for boulder #{boulder.id}: #{boulder.area_id} instead of #{area_id}" if (boulder.area_id != area_id.to_i)
+      else
+        if existing_boulder = Boulder.where(polygon: polygon).first
+          raise "boulder already exists (boulder_id=#{existing_boulder.id})" 
+        else
+          boulder = Boulder.new(area_id: area_id)
+        end
+      end
+      boulder.assign_attributes(
+        polygon: polygon
+      )
+
+      objects << boulder
+    end
+
+    objects
+  end
 end
