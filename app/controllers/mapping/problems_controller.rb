f5e1228b4e8ea93b35333802136fae181acda51a
Nicolas Mondollot <nmondollot@gmail.com>
2024-01-18 12:24:44 +0100

    Use scope

--- app/controllers/mapping/problems_controller.rb
+++ app/controllers/mapping/problems_controller.rb
@@ -14,9 +14,7 @@ class Mapping::ProblemsController < ApplicationController
       group_by(&:location_estimated).
       sort_by{|location, requests| requests.inject(0) {|sum, req| sum + req.problem.ascents.to_i } }.reverse
 
-    @problems = @area.problems.where(location: nil).
-      left_joins(:contribution_requests).where(contribution_requests: { id: nil }).
-      order(grade: :desc)
+    @problems = @area.problems.where(location: nil).without_contribution_request.order(grade: :desc)
 
     @tab = params[:tab] == "rest" ? :rest : :prio
   end
