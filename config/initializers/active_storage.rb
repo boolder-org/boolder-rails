749fbc1f56a9554dcbf27874b3e462f4f5691b80
Nicolas Mondollot <nmondollot@gmail.com>
2024-01-19 17:14:03 +0100

    Read-only access for prod bucket in dev env

--- config/initializers/active_storage.rb
+++ config/initializers/active_storage.rb
@@ -1,1 +1,5 @@
 Rails.application.config.active_storage.resolve_model_to_route = :rails_storage_proxy
+
+# quick hack to have read-only access for files stored on prod bucket
+S3_READONLY_KEY = "AKIA6FMTWZ62M437JUUM"
+S3_READONLY_SECRET = "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"
