99845bcd7d2ea41de5172a5d94a80f633161a69e
Nicolas Mondollot <nmondollot@gmail.com>
2024-01-22 18:07:08 +0100

    Normalize bleau_problem.name

--- app/models/bleau_problem.rb
+++ app/models/bleau_problem.rb
@@ -6,9 +6,19 @@ class BleauProblem < ApplicationRecord
   validates :steepness, inclusion: { in: Problem::STEEPNESS_VALUES }
   validates :grade, inclusion: { in: Problem::GRADE_VALUES }, allow_blank: true
   validates :circuit_letter, inclusion: { in: Problem::LETTERS.keys }, allow_blank: true
-  validate :validate_circuit_letter_is_numeric
+  validate :validate_circuit_number_is_numeric
 
-  def validate_circuit_letter_is_numeric
+  normalizes :name, with: -> s { s.strip.gsub(/\A-\z/, '').presence }
+
+  def name_with_fallback
+    if name.present?
+      name
+    else
+      I18n.t("problem.no_name")
+    end 
+  end
+
+  def validate_circuit_number_is_numeric
     return if circuit_number.blank? || circuit_number == Problem::LETTER_START || circuit_number == "A"
     if circuit_number.to_i < 1
       errors.add(:circuit_number, "must be a number or D (for Départ)")
