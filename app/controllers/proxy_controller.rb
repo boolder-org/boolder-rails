1df2ad3ba5b9154059c8cb2d3812d39bf3db17fb
Nicolas Mondollot <nmondollot@gmail.com>
2024-07-22 09:53:46 +0200

    404

--- app/controllers/proxy_controller.rb
+++ app/controllers/proxy_controller.rb
@@ -3,6 +3,8 @@ class ProxyController < ActiveStorage::BaseController
   include ActiveStorage::Streaming
   include ActiveStorage::DisableSession
 
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
   def show
     topo = Topo.find(params[:id])
     representation = topo.photo.variant(:medium)
@@ -10,5 +12,10 @@ class ProxyController < ActiveStorage::BaseController
     http_cache_forever public: true do
       send_blob_stream representation, disposition: params[:disposition]
     end
+  end
+
+  private
+  def not_found
+    head :not_found
   end
 end
