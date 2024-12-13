6f29019d478ba5448f8a02bf293310a3284cb05c
Nicolas Mondollot <nmondollot@gmail.com>
2024-12-13 09:35:15 +0100

    Load defaults

--- config/application.rb
+++ config/application.rb
@@ -9,10 +9,7 @@ Bundler.require(*Rails.groups)
 module Boolder
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
-
-    config.add_autoload_paths_to_load_path = false
-    config.active_support.cache_format_version = 7.1
+    config.load_defaults 7.2
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
