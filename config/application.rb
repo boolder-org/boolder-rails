2685c344803498bf95e55480be817a1a346e9a2c
Nicolas Mondollot <nmondollot@gmail.com>
2024-12-13 09:31:22 +0100

    Activate 7.1 defaults

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module Boolder
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.add_autoload_paths_to_load_path = false
+    config.active_support.cache_format_version = 7.1
+
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
