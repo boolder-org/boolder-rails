ff56ccd3b9e711f50123b77b3a6fc9d55bb75638
Nicolas Mondollot <nmondollot@gmail.com>
2024-12-16 12:24:35 +0100

    Fix brakeman warning

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -53,7 +53,7 @@ class Problem < ApplicationRecord
     scope color, -> { joins(:circuit).where(circuits: { color: color }) }
   end
 
-  scope :level, ->(i) { where("grade >= '#{i}a' AND grade < '#{i+1}a'").tap { raise unless i.in?(1..8) } }
+  scope :level, ->(i) { where("grade >= ? AND grade < ?", "#{i}a", "#{i + 1}a").tap { raise unless i.in?(1..8) } }
   scope :significant_ascents, -> { where("ascents >= ?", 20) }
   scope :exclude_bis, -> { where(circuit_letter: [ nil, "" ]) }
   scope :with_location, -> { where.not(location: nil) }
