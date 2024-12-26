6b9c2140fdede18f060ec45c0b5bba60ddc7e3a3
Nicolas Mondollot <nmondollot@gmail.com>
2024-12-26 17:13:57 +0100

    Remove hack (fixes #49)

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,9 +7,3 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
-
-// Hack to make algolia autocomplete work
-// (the npm package assumes window.process["NODE_ENV"] is present)
-window.process = {
-  env: { NODE_ENV: 'production' } // or 'development', depending on your needs
-};
