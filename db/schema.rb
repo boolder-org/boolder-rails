eeb1c6f87d7dafe5f669520c23fc279c43b15cd0
Nicolas Mondollot <nmondollot@gmail.com>
2024-12-26 12:28:34 +0100

    Schema for rails 8

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_04_091012) do
-
+ActiveRecord::Schema[8.0].define(version: 2024_09_04_091012) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
   enable_extension "pg_stat_statements"
   enable_extension "pg_trgm"
-  enable_extension "plpgsql"
   enable_extension "postgis"
   enable_extension "unaccent"
 
