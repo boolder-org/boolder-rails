f767abf05090f07cf58a0848a8dd878140fe6f86
Nicolas Mondollot <nmondollot@gmail.com>
2024-12-26 12:35:00 +0100

    No bugsnag locally

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -1,3 +1,3 @@
 Bugsnag.configure do |config|
-  config.api_key = Rails.application.credentials.dig(:bugsnag, :api_key)
+  config.api_key = Rails.application.credentials.dig(:bugsnag, :api_key) unless Rails.env.local?
 end
