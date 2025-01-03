a13395073536cfdcae5131ed367de4065efea378
Nicolas Mondollot <nmondollot@gmail.com>
2025-01-03 10:29:52 +0100

    Remove old authentication

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,26 +1,10 @@
 class Admin::BaseController < ApplicationController
+  include Authentication
   default_form_builder DefaultFormBuilder
   layout "admin"
-  before_action :authenticate, unless: -> { Rails.env.local? }
   before_action :set_cookie
 
   private
-  def authenticate
-    authenticate_or_request_with_http_basic("admin") do |id, password|
-      if accounts.has_key?(id.to_s)
-        session[:admin_user_name] = id
-        password == accounts[id.to_s]
-      end
-    end
-  end
-
-  def accounts
-    {
-      "nico"  => Rails.application.credentials.dig(:admin, :nico_password),
-      "emile" => Rails.application.credentials.dig(:admin, :emile_password),
-      "gael" => Rails.application.credentials.dig(:admin, :gael_password)
-    }
-  end
 
   # used by audited gem (see config/initializers/audited.rb)
   def authenticated_user
