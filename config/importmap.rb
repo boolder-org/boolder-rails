2be10e4dde90038b9f5c5fa70cb45e44358cc197
Nicolas Mondollot <nmondollot@gmail.com>
2025-01-03 16:33:02 +0100

    Remove old libs

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,6 +9,3 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "stimulus-chartjs", to: "https://ga.jspm.io/npm:stimulus-chartjs@5.0.0/dist/stimulus-chartjs.mjs"
 pin "@kurkle/color", to: "https://ga.jspm.io/npm:@kurkle/color@0.3.2/dist/color.esm.js"
 pin "chart.js/auto", to: "https://ga.jspm.io/npm:chart.js@4.3.0/auto/auto.js"
-
-pin "htm", to: "https://ga.jspm.io/npm:htm@3.1.1/dist/htm.module.js"
-pin "preact", to: "https://ga.jspm.io/npm:preact@10.15.1/dist/preact.module.js"
