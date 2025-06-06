6e1d53edb5daa5b9a963881b7f2374ce2d461e7c
Nicolas Mondollot <nmondollot@gmail.com>
2025-06-06 09:16:22 +0200

    Colors

--- app/helpers/admin/moderation/problems_helper.rb
+++ app/helpers/admin/moderation/problems_helper.rb
@@ -1,6 +1,8 @@
 module Admin::Moderation::ProblemsHelper
   def percentage_color(percentage)
     if percentage >= 0.98
+      "#1C630E" # green
+    elsif percentage >= 0.95
       "#1EB100" # green
     elsif percentage >= 0.9
       "#F7BA00" # yellow
