5a7db05d65d7ed66f5cb9946925af769a54fa0f4
Nicolas Mondollot <nmondollot@gmail.com>
2025-06-06 09:30:12 +0200

    Remove related problems

--- app/controllers/admin/problems_controller.rb
+++ app/controllers/admin/problems_controller.rb
@@ -51,12 +51,6 @@ class Admin::ProblemsController < Admin::BaseController
 
   def show
     set_problem
-
-    @related_problems = if @problem.bleau_problem.present?
-      Problem.where(bleau_info_id: @problem.bleau_problem&.related_ids)
-    else
-      []
-    end
   end
 
   def edit
